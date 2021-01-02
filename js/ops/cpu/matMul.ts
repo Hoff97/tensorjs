81642aa7ca78e88d9f7dfb901d6612a95ba01d80
Frithjof Winkelmann <frithjof97@web.de>
2021-01-02 13:41:45 +0000

    Move matmul to extra file

---
+++ js/ops/cpu/matMul.ts
@@ -0,0 +1,29 @@
+import CPUTensor from '../../tensor/cpu/tensor';
+
+export function matMul(a: CPUTensor, b: CPUTensor) {
+  if (a.shape.length !== 2 || b.shape.length !== 2) {
+    throw new Error('Matmul expects both operands to have rank 2');
+  }
+
+  if (a.shape[1] !== b.shape[0]) {
+    throw new Error('Matmul expects dimension 1 of operand 1 to equal dimension 0 of operand 2');
+  }
+
+  const m = a.shape[0];
+  const n = a.shape[1];
+  const o = b.shape[1];
+
+  const result = new CPUTensor([m, o]);
+
+  for (let i = 0; i < m; i += 1) {
+    for (let k = 0; k < o; k += 1) {
+      let res = 0;
+      for (let j = 0; j < n; j += 1) {
+        res += a.get([i, j]) * b.get([j, k]);
+      }
+      result.set([i, k], res);
+    }
+  }
+
+  return result;
+}
