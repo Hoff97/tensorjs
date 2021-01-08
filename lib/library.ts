0f6603d8b9b8c8937da6f1cd513efcbc835fd879
Frithjof Winkelmann <frithjof97@web.de>
2021-01-08 16:17:50 +0100

    Add more exports

--- lib/library.ts
+++ lib/library.ts
@@ -1,5 +1,8 @@
 import Tensor from './types';
 import WASMTensor from './tensor/wasm/tensor';
 import CPUTensor from './tensor/cpu/tensor';
+import GPUTensor from './tensor/gpu/tensor';
+import * as util from './util/convert'
+import { OnnxModel } from './onnx/model';
 
-export {Tensor, WASMTensor, CPUTensor};
+export {Tensor, WASMTensor, CPUTensor, GPUTensor, util, OnnxModel};
--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,36 @@ const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 const dist = path.resolve(__dirname, "dist");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    index: "./lib/library.ts"
-  },
-  output: {
-    path: dist,
-    filename: "[name].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new WasmPackPlugin({
-      crateDirectory: __dirname,
-    }),
-  ]
+module.exports = env => {
+  return {
+    mode: env === "production" ? "production" : "development",
+    entry: {
+      index: "./lib/library.ts"
+    },
+    output: {
+      path: dist,
+      filename: "[name].js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new WasmPackPlugin({
+        crateDirectory: __dirname,
+      }),
+    ]
+  }
 };
