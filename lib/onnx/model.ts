49697799d1b72aab486d778cdbcb9ac2e58b6beb
Frithjof Winkelmann <frithjof97@web.de>
2021-01-08 22:22:11 +0100

    Allow Int tensors of variable size

--- lib/onnx/model.ts
+++ lib/onnx/model.ts
@@ -1,12 +1,12 @@
 import Long from 'long';
 import {onnx} from 'onnx-proto';
-import { CPUTensor } from '../tensor/cpu/tensor';
 import Tensor from '../types';
 import { toCPU, toGPU, toWASM } from '../util/convert';
-import { TENSOR_FLOAT, TENSOR_INT64 } from './definitions';
+import { TENSOR_INT64 } from './definitions';
 import { OnnxNode } from './node';
 import { nodeResolve } from './resolve';
 import { Constants } from './types';
+import { createTensor } from './util';
 
 interface Intermediary {
   to: number[];
@@ -24,6 +24,7 @@ export class OnnxModel {
 
   private nodes: {[id: number]: OnnxNode} = {};
   private nodeIds: number[] = [];
+  private defaultReady: number[] = [];
 
   private intermediaries: {[name: string]: Intermediary} = {};
 
@@ -74,6 +75,10 @@ export class OnnxModel {
         }
         this.intermediaries[input].to.push(i);
       }
+
+      if (node.variableInputs === 0) {
+        this.defaultReady.push(i);
+      }
     }
   }
 
@@ -81,51 +86,11 @@ export class OnnxModel {
     for (let i = 0; i < initializer.length; i++) {
       const tensorProto = initializer[i];
 
-      const tensor = this.createTensor(tensorProto);
-      this.constants[tensorProto.name] = tensor;
-    }
-  }
-
-  private createTensor(tensorProto: onnx.ITensorProto): Tensor {
-    if (tensorProto.segment !== undefined && tensorProto.segment !== null) {
-      throw new Error('Handling of tensor proto segment not yet implemented');
-    }
-
-    let shape: number[] = tensorProto.dims as number[];
-    if (shape === undefined || shape === null) {
-      throw new Error('Tensor shape must be specified');
-    }
-    for (let i = 0; i < shape.length; i++) {
-      if (Long.isLong(shape[i])) {
-        shape[i] = (shape[i] as any).toNumber();
+      const tensor = createTensor(tensorProto);
+      this.constants[tensorProto.name] = {
+        type: tensorProto.dataType === TENSOR_INT64 ? 'Int' : 'float',
+        value: tensor
       }
-    }
-    if (shape.length === 0) {
-      shape = [1];
-    }
-
-    if (tensorProto.dataType === TENSOR_FLOAT) {
-      if (tensorProto.floatData && tensorProto.floatData.length > 0) {
-        return new CPUTensor(shape, tensorProto.floatData);
-      } else if (tensorProto.rawData && tensorProto.rawData.length > 0) {
-        const buffer = tensorProto.rawData.buffer.slice(tensorProto.rawData.byteOffset, tensorProto.rawData.byteOffset+tensorProto.rawData.byteLength);
-        const values = new Float32Array(buffer);
-        return new CPUTensor(shape, values);
-      } else {
-        throw new Error('Cant process float tensor without float or raw data');
-      }
-    } else if (tensorProto.dataType === TENSOR_INT64) {
-      if (tensorProto.rawData && tensorProto.rawData.length > 0) {
-        if (tensorProto.rawData.length > 8) {
-          throw new Error('Can only process single int64 values');
-        }
-        const value = Long.fromBytes(Array.from(tensorProto.rawData)).toNumber();
-        return new CPUTensor(shape, [value]);
-      } else {
-        throw new Error('Cant process int64 tensor without raw data');
-      }
-    } else {
-      throw new Error(`Handling of tensor type ${tensorProto.dataType} not yet implemented`);
     }
   }
 
@@ -139,7 +104,7 @@ export class OnnxModel {
       }
     }
 
-    const nodesReady: number[] = [];
+    const nodesReady: number[] = [...this.defaultReady];
 
     for (let i = 0; i < inputs.length; i++) {
       intermediaryRes[this.inputs[i].name] = {
@@ -165,11 +130,11 @@ export class OnnxModel {
       const nodeId = nodesReady.shift();
       const node = this.nodes[nodeId];
 
-      const inputs = [];
+      const inputs: Tensor[] = [];
       for (let i = 0; i < node.inputs.length; i++) {
         const input = node.inputs[i];
         if (this.constants[input] !== undefined) {
-          inputs.push(this.constants[input]);
+          inputs.push(this.constants[input].value);
         } else {
           const inter = intermediaryRes[input];
           inter.used++;
@@ -220,7 +185,9 @@ export class OnnxModel {
 
   async toCPU() {
     for (let i in this.constants) {
-      this.constants[i] = await toCPU(this.constants[i]);
+      if (this.constants[i].type !== 'Int') {
+        this.constants[i].value = await toCPU(this.constants[i].value);
+      }
     }
 
     for (let i of this.nodeIds) {
@@ -230,7 +197,9 @@ export class OnnxModel {
 
   async toWASM() {
     for (let i in this.constants) {
-      this.constants[i] = await toWASM(this.constants[i]);
+      if (this.constants[i].type !== 'Int') {
+        this.constants[i].value = await toWASM(this.constants[i].value);
+      }
     }
 
     for (let i of this.nodeIds) {
@@ -240,7 +209,9 @@ export class OnnxModel {
 
   async toGPU() {
     for (let i in this.constants) {
-      this.constants[i] = await toGPU(this.constants[i]);
+      if (this.constants[i].type !== 'Int') {
+        this.constants[i].value = await toGPU(this.constants[i].value);
+      }
     }
 
     for (let i of this.nodeIds) {
