6cc850461a96d1c0e37eabff6173d6d020dcd398
Frithjof Winkelmann <frithjof97@web.de>
2021-01-20 23:34:00 +0100

    Add tests for precompiled onnx nodes

---
+++ test/onnxPrecompiled.test.ts
@@ -0,0 +1,68 @@
+import { CPUTensor } from '../lib/tensor/cpu/tensor';
+import { OnnxModel } from '../lib/onnx/model';
+import { toCPU, toGPU, toWASM } from '../lib/util/convert';
+
+import { enabledTests } from './enabledTests';
+import { onnx } from 'onnx-proto';
+import { createTensor } from '../lib/onnx/util';
+import Tensor from '../lib/types';
+
+const run = false;
+
+const epsilon = 0.001;
+
+const opsetVersions = ['9'];
+
+if (run) {
+for (let opset of opsetVersions) {
+  describe(`Opset ${opset} precompiled`, () => {
+    for (let test of enabledTests) {
+      it(`Should work for operator ${test}`, async () => {
+        const resp = await fetch(`onnx/${opset}/${test}/model.onnx`);
+        const buffer = await resp.arrayBuffer();
+
+        const model = new OnnxModel(buffer);
+
+        const inputs: Tensor[] = [];
+        let i = 0;
+        while (true) {
+          const resp = await fetch(`onnx/${opset}/${test}/test_data_set_0/input_${i}.pb`);
+          if (resp.status !== 200) {
+            break;
+          }
+          const buffer = await resp.arrayBuffer();
+          const arr = new Uint8Array(buffer);
+          const tensorProto = onnx.TensorProto.decode(arr);
+          const tensor = createTensor(tensorProto);
+          inputs.push(tensor);
+          i++;
+        }
+
+        const respOut = await fetch(`onnx/${opset}/${test}/test_data_set_0/output_0.pb`);
+        const bufferOut = await respOut.arrayBuffer();
+        const arr = new Uint8Array(bufferOut);
+        const tensorProto = onnx.TensorProto.decode(arr);
+        const output = createTensor(tensorProto);
+
+
+        let out: Tensor;
+        const inputsDevice: Tensor[] = [];
+
+        await model.toGPU();
+        out = await toGPU(output);
+        for (let i = 0; i < inputs.length; i++) {
+          inputsDevice.push(await toGPU(inputs[i]));
+        }
+
+        await model.compileForInputs(inputsDevice);
+
+        const result1 = (await model.forward(inputsDevice))[0];
+        expect(await result1.compare(out, epsilon)).toBeTrue();
+        const result2 = (await model.forward(inputsDevice))[0];
+        expect(await result2.compare(out, epsilon)).toBeTrue();
+      });
+    }
+  });
+}
+
+}
