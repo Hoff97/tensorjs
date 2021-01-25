3ef5823520861bb4387dae967d6f66e80ec5c282
Frithjof Winkelmann <frithjof97@web.de>
2021-01-25 18:40:16 +0100

    Enable precompiling for softmax, unsqueeze

--- lib/onnx/nodes/softmax.ts
+++ lib/onnx/nodes/softmax.ts
@@ -1,3 +1,14 @@
+import { BinaryOpInfo } from "../../ops/gpu/binaryOperation";
+import { DivideOperation } from "../../ops/gpu/divide";
+import { ExpOperation } from "../../ops/gpu/exp";
+import { MaxOperation } from "../../ops/gpu/max";
+import { PoolInfo } from "../../ops/gpu/pool";
+import { SubtractOperation } from "../../ops/gpu/subtract";
+import { SumOperation } from "../../ops/gpu/sum";
+import { UnaryOpInfo } from "../../ops/gpu/unaryOperation";
+import { PrototypeTensor } from "../../tensor/cpu/prototype";
+import { CPUTensor } from "../../tensor/cpu/tensor";
+import { gpuConstructor, GPUTensor } from "../../tensor/gpu/tensor";
 import Tensor from "../../types";
 import { OnnxNode } from "../node";
 import { Attributes, Constants } from "../types";
@@ -5,13 +16,20 @@ import { Attributes, Constants } from "../types";
 export class SoftmaxNode extends OnnxNode {
   private axis?: number;
 
+  private compiled = false;
+  private maxOperation?: MaxOperation<GPUTensor>;
+  private subtractOperation?: SubtractOperation<GPUTensor>;
+  private expOperation?: ExpOperation<GPUTensor>;
+  private sumOperation?: SumOperation<GPUTensor>;
+  private divideOperation?: DivideOperation<GPUTensor>;
+
   constructor(attributes: Attributes, inputs: string[], outputs: string[], constants: Constants, onnxVersion: number) {
     super(attributes, inputs, outputs, constants, onnxVersion);
 
     this.axis = this.getAttributeInt("axis");
   }
 
-  async forward(inputs: Tensor[]): Promise<Tensor[]> {
+  async defaultForward(inputs: Tensor[]): Promise<Tensor[]> {
     const x = inputs[0];
 
     const shapeX = x.getShape();
@@ -41,5 +59,162 @@ export class SoftmaxNode extends OnnxNode {
     sum.delete();
 
     return [result.reshape(shapeX, false)];
+  }
+
+  async compiledForward(inputs: Tensor[]): Promise<Tensor[]> {
+    const x = inputs[0];
+
+    const shapeX = x.getShape();
+
+    let ax = this.axis;
+    if (ax === undefined) {
+      if (this.onnxVersion < 13) {
+        ax = 1;
+      } else {
+        ax = shapeX.length - 1;
+      }
+    }
+
+    const sh1 = shapeX.slice(0, ax).reduce((x,y) => x*y, 1);
+
+    const reshaped = x.reshape([sh1,-1], false);
+
+    const max = this.maxOperation.calc({X: reshaped as GPUTensor, axes: [1], keepDims: true});
+    const normalized = this.subtractOperation.calc({A: reshaped as GPUTensor, B: max, outputShape: reshaped.getShape()});
+    const exp = this.expOperation.calc({input: normalized});
+    const sum = this.sumOperation.calc({X: exp, axes: [1], keepDims: true});
+    const result = this.divideOperation.calc({A: exp as GPUTensor, B: sum, outputShape: reshaped.getShape()});
+
+    max.delete();
+    normalized.delete();
+    exp.delete();
+    sum.delete();
+
+    return [result.reshape(shapeX, false)];
+  }
+
+  async forward(inputs: Tensor[]): Promise<Tensor[]> {
+    if (!this.compiled) {
+      return this.defaultForward(inputs);
+    } else {
+      return this.compiledForward(inputs);
+    }
+  }
+
+  async staticForward(inputs: Tensor[], compile: boolean): Promise<{ outputs: (CPUTensor | PrototypeTensor)[]; }> {
+    if (this.allStaticCPU(inputs)) {
+      return this.defaultStaticForward(inputs);
+    }
+
+    const x = inputs[0];
+    const shapeX = x.getShape();
+
+    let ax = this.axis;
+    if (ax === undefined) {
+      if (this.onnxVersion < 13) {
+        ax = 1;
+      } else {
+        ax = shapeX.length - 1;
+      }
+    }
+    const sh1 = shapeX.slice(0, ax).reduce((x,y) => x*y, 1);
+    const sh2 = shapeX.slice(ax).reduce((x,y) => x*y, 1);
+    const newShape = [sh1, sh2];
+
+    const maxMemory = this.allocator.allocate(sh1);
+    const normalizedMemory = this.allocator.allocate(sh1*sh2);
+    const expMemory = this.allocator.allocate(sh1*sh2);
+    const sumMemory = this.allocator.allocate(sh1);
+    const memory = this.allocator.allocate(sh1*sh2);
+
+    if (compile) {
+      const [xMem] = this.getMemoryEntries(inputs);
+
+      const infoMax: PoolInfo = {
+        shapeX: [sh1, sh2],
+        widthX: xMem.width,
+        heightX: xMem.height,
+
+        shapeOutput: [sh1, 1],
+        widthOutput: maxMemory.width,
+        heightOutput: maxMemory.height,
+
+        axes: [1], keepDims: true
+      };
+      this.maxOperation.compile(infoMax);
+
+      const infoSub: BinaryOpInfo = {
+        shapeA: [sh1, sh2],
+        widthA: xMem.width,
+        heightA: xMem.height,
+
+        shapeB: [sh1, 1],
+        widthB: maxMemory.width,
+        heightB: maxMemory.height,
+
+        shapeOutput: [sh1, sh2],
+        widthOutput: normalizedMemory.width,
+        heightOutput: normalizedMemory.height,
+      };
+      this.subtractOperation.compile(infoSub);
+
+      const infoExp: UnaryOpInfo = {
+        shapeX: [sh1, sh2],
+        widthX: normalizedMemory.width,
+        heightX: normalizedMemory.height,
+
+        shapeOutput: [sh1, sh2],
+        widthOutput: expMemory.width,
+        heightOutput: expMemory.height
+      };
+      this.expOperation.compile(infoExp);
+
+      const infoSum: PoolInfo = {
+        shapeX: [sh1, sh2],
+        widthX: expMemory.width,
+        heightX: expMemory.height,
+
+        shapeOutput: [sh1, 1],
+        widthOutput: sumMemory.width,
+        heightOutput: sumMemory.height,
+
+        axes: [1], keepDims: true
+      };
+      this.sumOperation.compile(infoSum);
+
+      const infoDivide: BinaryOpInfo = {
+        shapeA: [sh1, sh2],
+        widthA: xMem.width,
+        heightA: xMem.height,
+
+        shapeB: [sh1, 1],
+        widthB: sumMemory.width,
+        heightB: sumMemory.height,
+
+        shapeOutput: [sh1, sh2],
+        widthOutput: memory.width,
+        heightOutput: memory.height,
+      };
+      this.divideOperation.compile(infoDivide);
+
+      this.compiled = true;
+    }
+
+    this.allocator.deallocate(maxMemory);
+    this.allocator.deallocate(normalizedMemory);
+    this.allocator.deallocate(expMemory);
+    this.allocator.deallocate(sumMemory);
+
+    return {
+      outputs: [new PrototypeTensor(x.getShape(), memory)]
+    };
+  }
+
+  initializeForCompiling(): void {
+    this.maxOperation = new MaxOperation(gpuConstructor, this.allocator);
+    this.subtractOperation = new SubtractOperation(gpuConstructor, this.allocator);
+    this.expOperation = new ExpOperation(gpuConstructor, this.allocator);
+    this.sumOperation = new SumOperation(gpuConstructor, this.allocator);
+    this.divideOperation = new DivideOperation(gpuConstructor, this.allocator);
   }
 }
--- lib/onnx/nodes/unsqueeze.ts
+++ lib/onnx/nodes/unsqueeze.ts
@@ -1,9 +1,17 @@
+import { CopyInfo, CopyOperation } from "../../ops/gpu/copy";
+import { PrototypeTensor } from "../../tensor/cpu/prototype";
+import { CPUTensor } from "../../tensor/cpu/tensor";
+import { gpuConstructor, GPUTensor } from "../../tensor/gpu/tensor";
 import Tensor from "../../types";
+import { getSize } from "../../util/shape";
 import { OnnxNode } from "../node";
 import { Attributes, Constants } from "../types";
 
 export class UnsqueezeNode extends OnnxNode {
   private axes: number[];
+
+  private compiled = false;
+  private operation?: CopyOperation<GPUTensor>;
 
   constructor(attributes: Attributes, inputs: string[], outputs: string[], constants: Constants, onnxVersion: number) {
     super(attributes, inputs, outputs, constants, onnxVersion);
@@ -31,8 +39,67 @@ export class UnsqueezeNode extends OnnxNode {
         newShape.push(1);
       }
 
-      return [x.reshape(newShape)];
+      if (!this.compiled) {
+        return [x.reshape(newShape)];
+      } else {
+        return [this.operation.calc({
+          input: x as GPUTensor,
+          outputShape: newShape
+        })];
+      }
     }
     throw new Error(`Unsqueeze with onnx version ${this.onnxVersion} not yet implemented`);
+  }
+
+  async staticForward(inputs: Tensor[], compile: boolean): Promise<{ outputs: (CPUTensor | PrototypeTensor)[]; }> {
+    if (this.allStaticCPU(inputs)) {
+      return this.defaultStaticForward(inputs);
+    }
+
+    if (this.onnxVersion < 13) {
+      const x = inputs[0];
+
+      const resultShape = [];
+      let axIx = 0;
+      for (let i = 0; i < x.getShape().length; i++) {
+        if (axIx < this.axes.length && this.axes[axIx] === i) {
+          resultShape.push(1);
+          axIx++;
+        }
+        resultShape.push(x.getShape()[i]);
+      }
+      if (this.axes[this.axes.length - 1] === x.getShape().length) {
+        resultShape.push(1);
+      }
+
+      const memory = this.allocator.allocate(getSize(resultShape));
+
+      if (compile) {
+        const [xMem] = this.getMemoryEntries(inputs);
+
+        const info: CopyInfo = {
+          shapeX: x.getShape(),
+          widthX: xMem.width,
+          heightX: xMem.height,
+
+          shapeOutput: resultShape,
+          widthOutput: memory.width,
+          heightOutput: memory.height,
+        };
+
+        this.operation.compile(info);
+
+        this.compiled = true;
+      }
+
+      return {
+        outputs: [new PrototypeTensor(resultShape, memory)]
+      };
+    }
+    throw new Error(`Slice is not implemented for onnx version ${this.onnxVersion}`);
+  }
+
+  initializeForCompiling(): void {
+    this.operation = new CopyOperation(gpuConstructor, this.allocator);
   }
 }
