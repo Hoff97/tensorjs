ff2cb02049c68efadf12da41133736f2ab431871
Frithjof Winkelmann <frithjof97@web.de>
2021-01-29 19:05:18 +0100

    Add test for float16 gpu tensors

--- test/gpu.test.ts
+++ test/gpu.test.ts
@@ -7,15 +7,22 @@ import testPool from "./pool";
 describe(`GPU create tensor`, () => {
   it('should get the same values back', async () => {
     const arr = new Float32Array([1.,2.,3.,4.]);
-    const tensor = new GPUTensor(arr, [2,2]);
+    const tensor = new GPUTensor(arr, [2,2], 32);
 
     expect(await tensor.getValues()).toEqual(arr);
+  });
+
+  it('should get the same values back when using half precision', async () => {
+    const arr = new Float32Array([1.,2.,3.,4.]);
+    const tensor = new GPUTensor(arr, [2,2], 16);
+
+    expect(await tensor.copy().getValues()).toEqual(arr);
   });
 });
 
 const constructor = (shape: ReadonlyArray<number>, values: number[]) => {
   const vals = Float32Array.from(values);
-  return new GPUTensor(vals, shape);
+  return new GPUTensor(vals, shape, 32);
 };
 
 /*testBasic('GPU', constructor);
