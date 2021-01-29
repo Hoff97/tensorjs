a2104b4fcb4efba55262a7b851708584a0bad82a
Frithjof Winkelmann <frithjof97@web.de>
2021-01-29 19:01:57 +0100

    Use standard precision for sum square by default

--- lib/ops/gpu/sumSquare.ts
+++ lib/ops/gpu/sumSquare.ts
@@ -1,5 +1,6 @@
 import { GPUTensorI } from "../../tensor/gpu/interface";
-import { PoolInput, PoolOperation } from "./pool";
+import { Precision } from "../../types";
+import { PoolInfo, PoolOperation } from "./pool";
 
 export class SumSquareOperation<GPUTensor extends GPUTensorI> extends PoolOperation<GPUTensor> {
   update(a: string, b: string): string {
@@ -7,5 +8,9 @@ export class SumSquareOperation<GPUTensor extends GPUTensorI> extends PoolOperat
   }
   init(res: string) {
     return `${res}*${res}`;
+  }
+
+  compile(info: PoolInfo, precision: Precision) {
+    super.compile(info, 32);
   }
 }
