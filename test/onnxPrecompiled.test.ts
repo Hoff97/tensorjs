6c1bc94be0021caf51adf890233ac9bdbb31e755
Frithjof Winkelmann <frithjof97@web.de>
2021-01-29 18:38:22 +0100

    Add tests for precompiled half precision onnx operators

--- test/onnxPrecompiled.test.ts
+++ test/onnxPrecompiled.test.ts
@@ -12,6 +12,18 @@ const epsilon = 0.001;
 
 const opsetVersions = ['9'];
 
+// These are not suitable since either their input is
+// outside of the range of float16 or the errors add up too much
+// TODO: Maybe overwrite the behavior of these operators
+// to use float32 in any case?
+const excludeHalfPrecision = new Set([
+  'test_softmax_large_number',
+  'test_upsample_nearest',
+  'test_reduce_sum_square_do_not_keepdims_random',
+  'test_reduce_sum_square_keepdims_random',
+  'test_reduce_sum_square_default_axes_keepdims_random'
+]);
+
 if (run) {
 for (let opset of opsetVersions) {
   describe(`Opset ${opset} precompiled`, () => {
@@ -60,6 +72,55 @@ for (let opset of opsetVersions) {
         const result2 = (await model.forward(inputsDevice))[0];
         expect(await result2.compare(out, epsilon)).toBeTrue();
       });
+
+      if (!excludeHalfPrecision.has(test)) {
+        it(`Should work for operator ${test} with half precision`, async () => {
+          const resp = await fetch(`onnx/${opset}/${test}/model.onnx`);
+          const buffer = await resp.arrayBuffer();
+
+          const model = new OnnxModel(buffer, {
+            precision: 16
+          });
+
+          const inputs: Tensor[] = [];
+          let i = 0;
+          while (true) {
+            const resp = await fetch(`onnx/${opset}/${test}/test_data_set_0/input_${i}.pb`);
+            if (resp.status !== 200) {
+              break;
+            }
+            const buffer = await resp.arrayBuffer();
+            const arr = new Uint8Array(buffer);
+            const tensorProto = onnx.TensorProto.decode(arr);
+            const tensor = createTensor(tensorProto);
+            inputs.push(tensor);
+            i++;
+          }
+
+          const respOut = await fetch(`onnx/${opset}/${test}/test_data_set_0/output_0.pb`);
+          const bufferOut = await respOut.arrayBuffer();
+          const arr = new Uint8Array(bufferOut);
+          const tensorProto = onnx.TensorProto.decode(arr);
+          const output = createTensor(tensorProto);
+
+
+          let out: Tensor;
+          const inputsDevice: Tensor[] = [];
+
+          await model.toGPU();
+          out = await toGPU(output, 32);
+          for (let i = 0; i < inputs.length; i++) {
+            inputsDevice.push(await toGPU(inputs[i], 16));
+          }
+
+          await model.compileForInputs(inputsDevice);
+
+          const result1 = (await model.forward(inputsDevice))[0];
+          expect(await result1.copy().compare(out, epsilon*30)).toBeTrue();
+          const result2 = (await model.forward(inputsDevice))[0];
+          expect(await result2.copy().compare(out, epsilon*30)).toBeTrue();
+        });
+      }
     }
   });
 }
