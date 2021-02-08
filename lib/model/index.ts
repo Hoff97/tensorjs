30e4aa43f077739d1fe2c0d8b6bd953d02996eb9
Frithjof Winkelmann <frithjof97@web.de>
2021-02-08 15:43:23 +0100

    Add l2, l1 regularization

--- lib/model/index.ts
+++ lib/model/index.ts
@@ -2,5 +2,6 @@ import {Module, Mode} from './module';
 import * as basic from './basic';
 import * as optimizer from './optimizer/index';
 import * as functional from './functional/index';
+import * as regularization from './regularization';
 
-export {Module, Mode, basic, optimizer, functional};
+export {Module, Mode, basic, optimizer, functional, regularization};
